064319ba0d57bc5c9f83e15594ad53b75c88ec53
Chris Barney <cbarney6@gmail.com>
2026-04-01 18:07:28 -0600

    Fix cached mappings data, add refresh button to name resolution

--- js/widget.js
+++ js/widget.js
@@ -4999,8 +4999,10 @@ async function loadManufacturerMappings() {
         const response = await fetch(`${SUPABASE_URL}/rest/v1/manufacturer_mappings?select=*&order=canonical_name`, {
             headers: {
                 'apikey': SUPABASE_ANON_KEY,
-                'Authorization': `Bearer ${SUPABASE_ANON_KEY}`
-            }
+                'Authorization': `Bearer ${SUPABASE_ANON_KEY}`,
+                'Cache-Control': 'no-cache'
+            },
+            cache: 'no-store'
         });
 
         if (!response.ok) {
