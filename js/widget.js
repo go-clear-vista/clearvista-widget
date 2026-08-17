274fff092f4dbf249a744b7088625bd286d79b40
Bubba <bubba@goclearvista.com>
2026-08-17 10:04:28 -0600

    fix: restore Vendor Direct admin name-resolution routing

--- js/widget.js
+++ js/widget.js
@@ -1278,6 +1278,9 @@ async function loadAdminResolutionData(dist) {
         } else if (dist === 'almo') {
             rpcName = 'get_almo_manufacturers';
             fieldName = 'manufacturer';
+        } else if (dist === 'vendordirect') {
+            rpcName = 'get_vendor_direct_manufacturers';
+            fieldName = 'manufacturer';
         } else {
             rpcName = 'get_adi_manufacturers';
             fieldName = 'manufacturer';
@@ -1305,6 +1308,7 @@ async function loadAdminResolutionData(dist) {
         if (dist === 'tdsynnex') aliasField = 'td_synnex_aliases';
         else if (dist === 'ingram') aliasField = 'ingram_micro_aliases';
         else if (dist === 'almo') aliasField = 'almo_aliases';
+        else if (dist === 'vendordirect') aliasField = 'vendor_direct_aliases';
         else aliasField = 'adi_global_aliases';
 
         // Build map of distributor name (uppercase) → canonical name
